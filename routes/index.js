1426ae56add1862e3eb71cb254387512824f98fb
Clayton M <clayton@txdocs.com>
2025-01-11 15:48:12 -0600

    Pulling Data and routes

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+﻿const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+router.use("/users", require("./user"));
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,21 @@
+﻿/* eslint-env node */
+const express = require("express");
+const app = express();
+
+const mongodb = require("./data/database");
+
+const port = process.env.PORT || 8080;
+
+app.use("/", require("./routes"));
+
+mongodb.initDb((err, db) => {
+  if (err) {
+    console.error("Database not running", err);
+  } else {
+    console.log("MongoDB Connected");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
